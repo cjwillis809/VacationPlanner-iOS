21c74c2f742aa8e59bcd029fe42e68c181c4866d
Brenda Maya <bmm@brenda-mayas-macbook-pro.local>
2016-03-08 02:50:43 -0600

    Created Tabbed Views and auto layout for Project and Budget Views

---
+++ VacationPlanner/MapViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MapViewController.swift
+//  VacationPlanner
+//
+//  Created by Brenda Maya on 3/7/16.
+//  Copyright © 2016 DePaul University. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+}
